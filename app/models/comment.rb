573496a88f28dce71904620a84907553e29ab6e9
adamits <ajwieme@gmail.com>
2019-04-15 09:04:05 -0400

    Use central timezone for comments, not UTC.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,8 @@
 class Comment < ActiveRecord::Base
   self.per_page = 10
+
+  def display_date
+    # We show comments in central time
+    created_at.in_time_zone("Central Time (US & Canada)").strftime("%m-%d-%Y at %I:%M %p")
+  end
 end
